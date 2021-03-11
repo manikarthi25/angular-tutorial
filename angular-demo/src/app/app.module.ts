11d945d5380d563f098b48e2a5423ad3efbc4875
unknown <manikarthi25@gmail.com>
2021-03-11 12:31:21 +0530

    Class Binding

--- angular-demo/src/app/app.module.ts
+++ angular-demo/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { InterpolationComponent } from './interpolation/interpolation.component';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { ClassBindingComponent } from './class-binding/class-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
     InterpolationComponent,
-    PropertyBindingComponent
+    PropertyBindingComponent,
+    ClassBindingComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-demo/src/app/class-binding/class-binding.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-class-binding',
+  template: `
+            <h1>Class Binding Example</h1> 
+            <h3 class = "text-success">Welcome Mani</h3>
+            <h3 [class.text-danger] = "hasError">Welcome Mani</h3>
+            <h3 [class] = "text-special" [class] = "isSuccess">Overrite css property by class propert</h3>
+            <h3 [ngClass] = "messageClasses">More than one class binding</h3>
+            `,
+  styles: [`
+                .text-success {
+                  color : green;
+                }
+                .text-danger {
+                  color : red;
+                }
+                .text-special {
+                  font-style:italic;
+                }
+              `]
+})
+export class ClassBindingComponent implements OnInit {
+
+  public hasError = true;
+  public isSpecial = true;
+  public isSuccess = true;
+
+  public messageClasses = {
+    "text-success" : !this.hasError,
+    "text-danger" : this.isSpecial,
+    "text-special" : this.isSuccess
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
