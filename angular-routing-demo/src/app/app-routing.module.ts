64ec2434b714ae8b56cecf86f788e0a1cee4dd88
unknown <manikarthi25@gmail.com>
2021-03-14 20:49:48 +0530

    Wildcard route and default rout set

--- angular-routing-demo/src/app/app-routing.module.ts
+++ angular-routing-demo/src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
-
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+/*
+** -> wild card route - Page not found
+'' -> Empty
+*/
 const routes: Routes = [
-  { path: 'employees', component:EmployeeListComponent},
-  { path: 'departments', component:DepartmentListComponent}
+  { path:'', redirectTo:'/departments', pathMatch:'full'},
+  { path: 'employees', component: EmployeeListComponent},
+  { path: 'departments', component: DepartmentListComponent},
+  { path: "**", component: PageNotFoundComponent}
+
 ];
 
 @NgModule({
@@ -13,4 +20,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [EmployeeListComponent, DepartmentListComponent]
+export const routingComponents = [EmployeeListComponent, DepartmentListComponent, PageNotFoundComponent]
--- angular-routing-demo/src/app/app.module.ts
+++ angular-routing-demo/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
