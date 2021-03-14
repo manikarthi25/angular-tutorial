1ec66ae31c41634ddfdf78fa9b9bbcc720016016
unknown <manikarthi25@gmail.com>
2021-03-14 20:36:47 +0530

    Routing Demo

---
+++ angular-routing-demo/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+
+const routes: Routes = [
+  { path: 'employees', component:EmployeeListComponent},
+  { path: 'departments', component:DepartmentListComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [EmployeeListComponent, DepartmentListComponent]
